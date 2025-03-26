21cfa03b92249a4cda257aea3a361b226289365e
Kofi K <kofi.k.otchere@gmail.com>
2025-03-26 08:47:24 +0000

    fifth a wip

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '3-Column-Card-Page-FrontEnd-Mentor', 
+  base: '/3-Column-Card-Page-FrontEnd-Mentor/', 
 })
